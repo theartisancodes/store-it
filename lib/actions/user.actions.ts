45ea1b03385c291630779c957c43fdec31cba66d
theartisancodes <kjoenzau@gmail.com>
2025-02-13 21:32:04 +0300

    fix falsy redirect after logging in

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -112,7 +112,6 @@ export const getCurrentUser = async () => {
     return parseStringify(user.documents[0]);
   } catch (error) {
     handleError(error, 'Failed to get current user');
-  } finally {
     redirect('/sign-in');
   }
 };
